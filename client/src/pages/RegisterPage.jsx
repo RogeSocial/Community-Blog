7e4f301b3fa74eec7c2c51b61e5820d1b5ca474f
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-01-19 16:20:03 +0100

    added frontend and styling for register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,36 @@
+import { Link } from 'react-router-dom';
+import '../styling/account.css';
+
 function RegisterPage() {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="register-container">
+      <h2>Register account</h2>
+      <label>
+        Full Name:
+        <input type="text" className="input-field" />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" className="input-field" />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" className="input-field" />
+      </label>
+      <br />
+      <label>
+        Confirm Password:
+        <input type="password" className="input-field" />
+      </label>
+      <br />
+      <button className="register-button">Register</button>
+      <p>
+        Already have an account? <Link to="/login" className="login-link">Login here</Link>.
+      </p>
     </div>
-  )
+  );
 }
 
 export default RegisterPage;
